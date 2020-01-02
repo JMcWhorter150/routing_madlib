cfc8eb9d5d394ebf7352840ddefa08c507015bed
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-01-02 14:01:40 -0500

    Adds part two vip functionality

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const http = require('http');
 
+
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
+
+    const vips = ["Joe", "Eric", "Allyson"];
     let reqUrl = req.url.slice(1);
     let content = `Hello, ${reqUrl}`;
+    
+    if (vips.includes(reqUrl)) {
+        content = `How wonderfully splendid it is to be in your presence again, ${reqUrl}! You look magnificent today!`;
+    }
     res.end(content);
 })
 
